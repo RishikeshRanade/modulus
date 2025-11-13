fix to the scaling factor calculation

examples/structural_mechanics/crash_domino/src/crash_datapipe.py
@@ -173,17 +173,17 @@ class CrashDataset(Dataset):
 
         # Add the parameters to the dictionary
         return {
-            "stl_coordinates": np.float32(stl_vertices),
-            "stl_centers": np.float32(stl_centers),
-            "stl_faces": np.float32(mesh_indices_flattened),
-            "stl_areas": np.float32(stl_sizes),
-            "surface_mesh_centers": np.float32(surface_coordinates),
+            "stl_coordinates": np.float32(surface_coordinates), # np.float32(surface_coordinates)
+            # "stl_centers": np.float32(stl_centers), # surface centers
+            # "stl_faces": np.float32(mesh_indices_flattened), #
+            # "stl_areas": np.float32(stl_sizes),
+            "surface_mesh_centers": np.float32(surface_coordinates), # (N, 3) coordinates, x, y, z
             # "surface_normals": np.float32(surface_normals),
             # "surface_areas": np.float32(surface_sizes),
-            "surface_fields": np.float32(surface_fields),
-            "surface_features": np.float32(surface_coordinates), # This can be thickness and material properties on nodes
-            "geometry_features": np.float32(stl_vertices), # This can be thickness and material properties on nodes
-            "timesteps": np.float32(timesteps),
+            "surface_fields": np.float32(surface_fields), # (N, 3) acceleration
+            "surface_features": np.float32(surface_coordinates), # (N, 1) thickness, This can be thickness and material properties on nodes
+            "geometry_features": np.float32(stl_vertices), # This can be thickness and material properties on nodes # rename tostl_features
+            "timesteps": np.float32(timesteps), # t = [0, 1, 2, 3, 4, 5, 6]
             "filename": cfd_filename,
             "global_params_values": global_params_values,
             "global_params_reference": global_params_reference,

physicsnemo/datapipes/cae/cae_dataset.py
@@ -1205,8 +1205,10 @@ def compute_mean_std_min_max(
             # Compute batch statistics
             batch_mean = field_data.mean(axis=axis)
             batch_M2 = ((field_data - batch_mean) ** 2).sum(axis=axis)
-            batch_n = field_data.shape[0] * field_data.shape[1]
 
+            if transient:
+                batch_n = field_data.shape[0] * field_data.shape[1]
+            
             # Update running mean and M2 (Welford's algorithm)
             delta = batch_mean - mean[field_key]
             N[field_key] += batch_n  # batch_n should also be torch.int64
@@ -1241,6 +1243,10 @@ def compute_mean_std_min_max(
         for field_key in field_keys:
             field_data = data[field_key]
 
+            if transient:
+                if len(field_data.shape) == 3:
+                    field_data = field_data.reshape(field_data.shape[0] * field_data.shape[1], field_data.shape[-1])
+                
             batch_n = field_data.shape[0]
 
             # # Update min/max

physicsnemo/datapipes/cae/domino_datapipe_transient.py
@@ -865,7 +865,7 @@ class DoMINODataPipe(Dataset):
         # This function gets information about the surface scale,
         # and decides what the surface grid will be:
 
-        stl_coordinates = data_dict["stl_coordinates"]
+        stl_coordinates = data_dict["stl_coordinates"] # (N, 3) coordinates, x, y, z
 
         s_min, s_max, surf_grid = self.compute_stl_scaling_and_surface_grids()
 
@@ -915,17 +915,18 @@ class DoMINODataPipe(Dataset):
             normed_vertices = data_dict["stl_coordinates"]
 
         # For SDF calculations, make sure the mesh_indices_flattened is an integer array:
-        mesh_indices_flattened = data_dict["stl_faces"].to(torch.int32)
+        mesh_indices_flattened = data_dict["stl_faces"].to(torch.int32) # Make this optional
 
         # Compute signed distance function for the surface grid:
+        # Make this optional
         sdf_surf_grid, _ = signed_distance_field(
             mesh_vertices=normed_vertices,
             mesh_indices=mesh_indices_flattened,
             input_points=surf_grid,
             use_sign_winding_number=True,
         )
-        return_dict["sdf_surf_grid"] = sdf_surf_grid
-        return_dict["surf_grid"] = surf_grid
+        return_dict["sdf_surf_grid"] = sdf_surf_grid # Make this optional
+        return_dict["surf_grid"] = surf_grid # Make this optional
 
         # Store this only if normalization is active:
         if self.config.normalize_coordinates: